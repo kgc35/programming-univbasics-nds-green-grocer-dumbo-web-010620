93587b3124a1b27cea8f9a69371d11c239db1bf9
Kent Chan <kntchn@gmail.com>
2020-01-03 17:55:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,28 +21,56 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   num_items = cart.length;
+  new_cart =[];
+  i=0;
 
-  num_items.times do |index|
-    item_name = cart[index][:item]
-    item_price = cart[index][:price]
-    item_clearance = cart[index][:clearance]
-    new_cart =[];
+  while i< num_items do
+    item_name = cart[i][:item]
+    item_price = cart[i][:price]
+    item_clearance = cart[i][:clearance]
+
   # If there's no key for this number, add the number as a key and assign it
   # a new Array for holding future snacks with that price
-  if find_item_by_name_in_collection(item_name, new_cart) == nil
-    new_cart.push({:item => item_name, :price => item_price, :clearance => item_clearance, :count => 1})
+  sought_item = find_item_by_name_in_collection(item_name, new_cart)
+  if sought_item == nil
+    sought_item = {:item => item_name, :price => item_price, :clearance => item_clearance, :count => 1}
+    new_cart << sought_item
   else
-    result[snack_price] += 1
+    sought_item[:count] += 1 ;
   end
-  i += 1
+  i+=1;
 end
-
+new_cart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0
+  while counter < coupons.length
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    couponed_item_name = "#{coupons[counter][:item]} W/ COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupon[counter][:num]
+        cart_item[:count] -= coupons[:counter][:num]
+      else
+        cart_item_with_coupon ={
+          :item => couponed_item_name,
+          :price => coupons[counter][:cost]/ coupons[counter][:num],
+          :count => coupons[counter][:num],
+          :clearance => cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
+      end
+    end
+    counter += 1
+  end
+  cart
 end
 
 def apply_clearance(cart)
